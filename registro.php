4b33c3122bb8eae02e0d2ef3e03ae63b3c0cebed
Mauro Triaca <maurotriaca7@gmail.com>
2021-11-13 00:44:12 -0300

    estilos y html

---
+++ registro.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registro</title>
+	<link rel="stylesheet" type="text/css" href="public/main.css">
+</head>
+<body>
+	<h1></h1>
+	<form class="formulario">
+		<p>Nombre<input type="tex"></p>
+		<p>Apellido<input type="tex"></p>
+		<p>Contraseña<input type="password"></p>
+		<p><input type="button" value="register"></p>
+
+
+	</form>
+</body>
+</html>
